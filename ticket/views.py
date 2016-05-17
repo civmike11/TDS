568320ccdd1f65ab4ea34b97385bfd1e2234d995
Michael Moore <civmike11@gmail.com>
2016-05-16 22:57:02 -0500

    Update index

--- ticket/views.py
+++ ticket/views.py
@@ -1,11 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from .models import Ticket
 # Create your views here.
 
 
+@login_required
 def index(request):
-    return HttpResponse("Ticket Home")
+    active_tickets = Ticket.get_all_active_tickets()
+    context = {'active_tickets': active_tickets}
+
+    return render(request, 'ticket/index.html', context)
 
 
+@login_required
+def all(request):
+    return HttpResponse("All Portion")
+
+
+@login_required
 def detail(request):
     return HttpResponse("Detail Portion")
